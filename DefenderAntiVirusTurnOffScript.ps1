176316bd9c855359ac2db82bcab69c3c3aa2d10f
Osman Kandemir <juanter53@hotmail.com>
2023-03-25 20:46:17 +0300

    Create DefenderAntiVirusTurnOffScript.ps1

---
+++ DefenderAntiVirusTurnOffScript.ps1
@@ -0,0 +1,2 @@
+Set-NetFirewallProfile -Enabled False
+Set-MpPreference -DisableIntrusionPreventionSystem $true -DisableIOAVProtection $true -DisableRealtimeMonitoring $true -DisableScriptScanning $true -EnableControlledFolderAccess Disabled -EnableNetworkProtection AuditMode -Force -MAPSReporting Disabled -SubmitSamplesConsent NeverSend
